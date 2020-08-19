1ee1bb24f0ba56408af20c6266c88e7c766e12eb
jeongha-joo <jha970107@gmail.com>
2020-08-19 20:28:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Nav extends Component {
+  state = {
+    list: [],
+  }
+  componentDidMount() {
+    fetch('list.json')
+      .then(function (result) {
+        return result.json()
+      })
+      .then(
+        function (json) {
+          console.log(json)
+          this.setState({ list: json })
+        }.bind(this),
+      )
+  }
+  render() {
+    var listTag = []
+    for (var i = 0; i < this.state.list.length; i++) {
+      var li = this.state.list[i]
+      listTag.push(
+        <li key={li.id}>
+          <a
+            href={li.id}
+            data-id={li.id}
+            onClick={function (e) {
+              e.preventDefault()
+              console.log('trigger')
+              this.props.onClick(e.target.dataset.id)
+            }.bind(this)}
+          >
+            {li.title}
+          </a>
+        </li>,
+      )
+    }
+    return (
+      <nav>
+        <ul>{listTag}</ul>
+      </nav>
+    )
+  }
 }
 
-export default App;
+class Article extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    )
+  }
+}
+
+class App extends Component {
+  state = {
+    article: { title: 'Welcome', desc: 'Hello, React & Ajax' },
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>WEB</h1>
+        <Nav
+          onClick={function (id) {
+            fetch(id + '.json')
+              .then(function (result) {
+                return result.json()
+              })
+              .then(
+                function (json) {
+                  this.setState({
+                    article: { title: json.title, desc: json.desc },
+                  })
+                }.bind(this),
+              )
+          }.bind(this)}
+        ></Nav>
+        <Article
+          title={this.state.article.title}
+          desc={this.state.article.desc}
+        ></Article>
+      </div>
+    )
+  }
+}
+
+export default App
